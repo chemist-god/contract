7b68e696f9a87350aeca7526b7210c2ab540138a
chemist-god <godtenkorang72@gmail.com>
2025-04-27 20:47:43 +0000

    Deploy Couter contract to Lisk Sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    lisk: {
+      url: "https://rpc.sepolia-api.lisk.com", // Lisk Sepolia RPC
+      chainId: 4202,
+      accounts: [`0x${process.env.PRIVATE_KEY}`], 
+    },
+  },
 };
