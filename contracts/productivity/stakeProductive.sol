0c9adfe266c592df88c00ba13bf7c0481b722ff0
chemist-god <godtenkorang72@gmail.com>
2025-08-30 23:55:21 +0000

    the stake func

--- contracts/productivity/stakeProductive.sol
+++ contracts/productivity/stakeProductive.sol
@@ -35,4 +35,13 @@ contract ProductivityStaker {
         _;
     }
 
+    // Stake funds to activate productivity mode
+    function stake(uint256 amount) external payable {
+        require(msg.value == amount, "Incorrect ETH sent");
+        require(amount > 0, "Stake must be positive");
+
+        stakes[msg.sender] += amount;
+        totalStaked += amount;
+        emit Staked(msg.sender, amount);
+    }
 }
