222801a5e1e1c8cd4be6797a2a17e703362f02f2
chemist-god <godtenkorang72@gmail.com>
2025-09-05 23:56:54 +0000

    farmer registration and vetting functions

--- contracts/FarmVetting.sol
+++ contracts/FarmVetting.sol
@@ -23,4 +23,23 @@ contract FarmerVetting {
     constructor() {
         admin = msg.sender;
     }
+
+    function registerFarmer(string memory _name, string memory _location) public {
+        Farmer storage f = farmers[msg.sender];
+        f.name = _name;
+        f.location = _location;
+        f.reputationScore = 0;
+        f.isVetted = false;
+        f.isEligible = false;
+        farmerList.push(msg.sender);
+    }
+
+    function vetFarmer(address _farmer, uint _score) public onlyAdmin {
+        require(bytes(farmers[_farmer].name).length > 0, "Farmer not registered");
+        farmers[_farmer].reputationScore = _score;
+        farmers[_farmer].isVetted = true;
+        farmers[_farmer].isEligible = _score >= 70; // Threshold for eligibility
+    }
+
+    
 }
