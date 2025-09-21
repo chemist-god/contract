a37f54c51e7e6d078cbbbeaf714865703e683fd1
chemist-god <godtenkorang72@gmail.com>
2025-09-21 23:46:59 +0000

    writeNote and getNoteCount functions in NoteRegistry contract

--- contracts/Hustle/Noteregistry.sol
+++ contracts/Hustle/Noteregistry.sol
@@ -19,5 +19,25 @@ contract NoteRegistry {
         string contentCID
     );
 
-   
+    function writeNote(
+        string memory _title,
+        string memory _contentCID,
+        string memory _category
+    ) external {
+        notes.push(Note({
+            title: _title,
+            contentCID: _contentCID,
+            category: _category,
+            timestamp: block.timestamp,
+            author: msg.sender
+        }));
+
+        emit NoteWritten(notes.length - 1, msg.sender, _category, _contentCID);
+    }
+
+    function getNoteCount() external view returns (uint256) {
+        return notes.length;
+    }
+
+    
 }
