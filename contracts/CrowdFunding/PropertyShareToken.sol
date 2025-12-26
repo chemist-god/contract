942e8c2485fb6c19ff5310d87e5a29330e8056a7
TENKORANG GODFRED <godtenkorang72@gmail.com>
2025-12-26 23:56:46 +0000

    PropertyShareToken contract for fractional property ownership

---
+++ contracts/CrowdFunding/PropertyShareToken.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address a) external view returns (uint256);
+    function transfer(address r, uint256 a) external returns (bool);
+    function transferFrom(address s, address r, uint256 a) external returns (bool);
+    function allowance(address o, address s) external view returns (uint256);
+    function approve(address s, uint256 a) external returns (bool);
+}
+
+/// @title PropertyShareToken
+/// @notice ERC20-style token representing fractional ownership in a single property.
+contract PropertyShareToken {
+    string public name;
+    string public symbol;
+    uint8 public constant decimals = 18;
+
+    address public manager;      // Crowdfund manager
+    address public sponsor;      // Property sponsor
+
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    
+}
